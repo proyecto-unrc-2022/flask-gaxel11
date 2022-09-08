c018e4e62e410be32dc08423fc98cddac04f0439
gaxel11 <axel_maicco@hotmail.com>
2022-09-08 03:33:59 -0300

    post and get scenarios

--- features/steps/user.py
+++ features/steps/user.py
@@ -1,21 +1,68 @@
+from email import header
 import json
 from behave import *
 from application import USERS
 
+#Scenario: Retrieve a customers details
 @given('some users are in the system')
 def step_impl(context):
     USERS.update({'jasonb': {'name': 'Jason Bourne'}})
 
-@when(u'I retrieve the customer \'jasonb\'')
+@when('I retrieve the customer \'jasonb\'')
 def step_impl(context):
     context.page = context.client.get('/users/{}'.format('jasonb'))
     assert context.page
 
-@then(u'I should get a \'200\' response')
+@then('I should get a \'200\' response')
 def step_impl(context):
     assert context.page.status_code is 200
 
-@then(u'the following user details are returned')
+@then('the following user details are returned')
 def step_impl(context):
     # assert context.table[0].cells[0] in context.page.text
     assert "Jason Bourne" in context.page.text
+
+
+#Scenario: Agregar nuevo usuario
+@given('un usuario \'test\' con nombre \'Esto Esuntest\' que no existe en USERS')
+def step_given(self) :
+   assert (not USERS.get('test'))  
+
+@given('se lo recibe a traves de un POST')
+def step_given(context) :
+    context.headers = {'Content-Type': 'application/json'}
+    context.page = context.client.post('/users', data=json.dumps({"test": "Esto Esuntest"}), headers=context.headers)
+    assert context.page
+    
+@when('yo agrego ese usuario nuevo')
+def step_when(self) :
+    USERS.update({'test': {'name': 'Esto Esuntest'}})
+    assert (USERS.get('test'))  
+    
+@then('deberia responder con un \'200\'')
+def step_then(context) :
+    assert context.page.status_code is 200
+  
+
+@then('mostrar el mensaje: Created user')
+def step_then(context) :
+    assert "Created user" in context.page.text
+
+
+#Scenario: Mostrar todos los usuarios
+@given('un usuario ingresa al sistema')
+def step_given(self) :
+    USERS.update({'jasonb': {'name': 'Jason Bourne'}})
+    USERS.update({'test': {'name': 'Esto Esuntest'}})
+
+@given('recibo una solicitud GET')
+def step_given(context) :
+    context.page = context.client.get('/users')
+    assert context.page
+    
+@then('mostrar todos los usuarios')
+def step_then(context) :
+    print(USERS)
+    print(context.page.text)
+    assert USERS == json.loads(context.page.text)
+
--- features/user.feature
+++ features/user.feature
@@ -7,3 +7,20 @@ Feature: Handle storing, retrieving and deleting customer details # test/feature
     And the following user details are returned:
       | name        |
       | Jason Borne |
+
+  Scenario: Agregar nuevo usuario
+      Given un usuario 'test' con nombre 'Esto Esuntest' que no existe en USERS
+      And se lo recibe a traves de un POST
+      When yo agrego ese usuario nuevo
+      Then deberia responder con un '200'
+      And mostrar el mensaje: Created user
+
+  Scenario: Mostrar todos los usuarios
+      Given un usuario ingresa al sistema
+      And recibo una solicitud GET
+      Then deberia responder con un '200'
+      And mostrar todos los usuarios
+      | name        |
+      | Jason Borne |
+      | Esto Esuntest |
+          
